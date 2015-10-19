e97ad7f207da6f18bdf1aceaf1f458844067fe2b
frittie <frida.svensson@telia.com>
2015-10-19 09:51:09 +0200

    Mobile-menu

--- themes/scibase/components/mobileMenu.php
+++ themes/scibase/components/mobileMenu.php
@@ -1,20 +1,4 @@
 <button></button>
 
-<nav>
-	<ul class="parent-menu"><p>Huvudmeny</p>
-		<li><p>Menyval</p>
-			<ul class="submenu"><p>Undermeny</p>
-				<li><p>Menyval - undermeny </p></li>
-				<li><p>Menyval - undermeny </p></li>
-				<li><p>Menyval - undermeny </p></li>
-				<li><p>Menyval - undermeny </p></li>
-			</ul>
-		</li>
-		<li><p>Menyval</p></li>
-		<li><p>Menyval</p></li>
-		<li><p>Menyval</p></li>
-		<li><p>Menyval</p></li>
-	</ul>
-</nav>
 
 
