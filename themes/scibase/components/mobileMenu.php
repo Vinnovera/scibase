d281edc982060ab711f5489f4b8f36d6428e6d60
Per Lundgren <per.lundgren@vinnovera.se>
2015-10-29 12:58:40 +0100

    add mobile menu markup
    Signed-off-by: Per Lundgren <per.lundgren@vinnovera.se>

--- themes/scibase/components/mobileMenu.php
+++ themes/scibase/components/mobileMenu.php
@@ -1,33 +1,25 @@
-<!-- mp-menu -->
-<nav id="mp-menu" class="mp-menu">
-	<div class="mp-level">
-		<h2>Mainmenu</h2>
-		<ul>
-			<li>
-				<a href="index.php"> &laquo; Text</a>
-				<div class="mp-level">
-					<h2>Submenu</h2>
-					<ul>
-						<li>
-							<a href="#">Text - Submenu</a>
-						</li>
-						<li>
-							<a href="#">Text - Submenu</a>
-						</li>
-						<li>
-							<a href="#">Text - Submenu</a>
-						</li>
-					</ul>
-				</div>
-			</li>
-			<li><a href="#"> &laquo; Text</a></li>
-			<li><a href="#"> &laquo; Text</a></li>
-			<li><a href="#"> &laquo; Text</a></li>
-			<li><a href="#"> &laquo; Text</a></li>
-		</ul>
-	</div>
-</nav>
-<!-- /mp-menu -->
+<?php
+
+$mobileMenu = array(
+	'theme_location'  => 'mobileMenu',
+	'container'       => 'nav',
+	'container_class' => 'mp-menu',
+	'container_id'    => 'mp-menu',
+	'menu_class'      => 'menu',
+	'menu_id'         => '',
+	'echo'            => true,
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '',
+	'link_after'      => '',
+	'items_wrap'      => '<div class="mp-level"><h2>Main Menu</h2><ul id="%1$s" class="%2$s">%3$s</ul></div>',
+	'depth'           => 0,
+	'walker'          => new mobileMenu_walker_nav_menu
+);
+
+wp_nav_menu( $mobileMenu );
+
+?>
 <div class="menu-bar">
 	<div class="menu-button">				
 		<a href="#" id="trigger" class="menu-trigger"></a>
