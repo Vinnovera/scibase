a355fc57a2afd7720e63e53dcdcfd04421367b4f
Per Lundgren <per.lundgren@vinnovera.se>
2015-10-29 15:06:09 +0100

    fix meta tag to make site responsive
    Signed-off-by: Per Lundgren <per.lundgren@vinnovera.se>

--- themes/scibase/study.php
+++ themes/scibase/study.php
@@ -10,7 +10,7 @@ Template Name: Landing Page Study
 <head>
 <meta charset="<?php bloginfo('charset'); ?>" />
 <title><?php wp_title('|'); ?></title>
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" http-equiv="X-UA-Compatible" content="width=device-width, initial-scale=1, IE=edge,chrome=1">
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/ui/js/lib/modernizr.custom.15992.js"></script>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>">
