d8e2bd71b7cf97fdd63e93252434d6a6269fad3a
Per Lundgren <per.lundgren@vinnovera.se>
2016-02-09 13:41:25 +0100

    add google tag manager to theme
    Signed-off-by: Per Lundgren <per.lundgren@vinnovera.se>

--- themes/scibase/functions.php
+++ themes/scibase/functions.php
@@ -606,6 +606,13 @@ class mobileMenu_walker_nav_menu extends Walker_Nav_Menu {
 	  
 }
 
+add_action('wp_footer', 'ga_tag_manager');
 
+function ga_tag_manager() {
+	
+	$code = "<!-- Google Tag Manager --><noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-WNNWP9'height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WNNWP9');</script><!-- End Google Tag Manager -->"; 
+	echo $code;
+
+}
 
 ?>
